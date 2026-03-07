2d99b97b7c89df82cb1cea09a1859d46fb25d60d
IsaacSheardRobot <imsheard@mit.edu>
2026-03-07 16:05:30 -0500

    DS.3 Complete Code
    Needed to include all of the parameters and added a plot for Thrust because it wasn't included but asked of us. Also refined the scan and found our optimal values.

--- UEFC1_codes/DS_scan_ARS.py
+++ UEFC1_codes/DS_scan_ARS.py
@@ -95,58 +95,75 @@ def scan_ARS(aircraft: UEFC,
         # get_ipython().run_line_magic('matplotlib', 'qt')
         marker=(8,2,0)  # 8-sided asterisk
 
-        fig, ax = plt.subplots(3, 2, figsize=(9, 9)) #, dpi=150
+        fig, ax = plt.subplots(4, 2, figsize=(10, 12))
 
         # Helper function for consistent contour plots
-        def contourf(ax,vals,levels):
+        def contourf(ax, vals, levels):
             cs = ax.contourf(ARvals, Svals, vals, levels=levels)
             cs.set_edgecolor("black")
             clabels = ax.clabel(cs, colors="black", fontsize=8.0, fmt="%.2f")
             [txt.set_bbox(dict(facecolor='white', edgecolor='none', pad=0)) for txt in clabels]
             ax.plot(ARopt, Sopt, marker=marker, color="red", markersize=20)
+            return cs
 
         # Objective function: velocity
         levels = np.linspace(np.min(objvals[objvals > 0]), np.max(objvals), 9)
-        contourf(ax[0,0],objvals,levels)
-        #ax[0,0].set_xlabel("Aspect ratio (-)")
-        ax[0,0].set_ylabel("Wing area ($m^2$)")
-        ax[0,0].set_title("Objective = V (m/s)")
+        cs = contourf(ax[0,0], objvals, levels)
+        fig.colorbar(cs, ax=ax[0,0])
+        ax[0,0].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[0,0].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[0,0].set_title("Objective = V (m/s)", color="black", fontweight="bold")
 
-        # Objective function: payload fraction
+        # Payload fraction
         levels = np.linspace(np.min(pfvals[pfvals > 0]), np.max(pfvals), 9)
-        contourf(ax[0,1],pfvals,levels)
-        #ax[0,0].set_xlabel("Aspect ratio (-)")
-        ax[0,1].set_title("Payload fraction (-)")
+        cs = contourf(ax[0,1], pfvals, levels)
+        fig.colorbar(cs, ax=ax[0,1])
+        ax[0,1].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[0,1].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[0,1].set_title("Payload fraction (-)", color="black", fontweight="bold")
 
         # Lift coefficient
         levels = np.linspace(np.min(CLvals[CLvals > 0]), np.max(CLvals), 11)
-        contourf(ax[1,0],CLvals,levels)
-        #ax[1,0].set_xlabel("Aspect ratio (-)")
-        ax[1,0].set_ylabel("Wing area ($m^2$)")
-        ax[1,0].set_title("Lift coefficient (-)")
+        cs = contourf(ax[1,0], CLvals, levels)
+        fig.colorbar(cs, ax=ax[1,0])
+        ax[1,0].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[1,0].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[1,0].set_title("Lift coefficient, $C_L$ (-)", color="black", fontweight="bold")
 
         # Drag coefficient
         levels = np.linspace(np.min(CDvals[CDvals > 0]), np.max(CDvals), 11)
-        contourf(ax[1,1],CDvals,levels)
-        #ax[1,1].set_xlabel("Aspect ratio (-)")
-        #ax[1,1].set_ylabel("Wing area ($m^2$)")
-        ax[1,1].set_title("Drag coefficient (-)")
+        cs = contourf(ax[1,1], CDvals, levels)
+        fig.colorbar(cs, ax=ax[1,1])
+        ax[1,1].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[1,1].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[1,1].set_title("Drag coefficient, $C_D$ (-)", color="black", fontweight="bold")
+
+        # Required thrust
+        levels = np.linspace(np.min(Tvals[Tvals > 0]), np.max(Tvals), 11)
+        cs = contourf(ax[2,0], Tvals, levels)
+        fig.colorbar(cs, ax=ax[2,0])
+        ax[2,0].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[2,0].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[2,0].set_title("Required thrust, $T$ (N)", color="black", fontweight="bold")
 
         # Maximum thrust
-        # Levels here are the same as for Tvals, so thast thrust contours can be
-        # compared more easily
         levels = np.linspace(np.min(Tvals[Tvals > 0]), np.max(Tvals), 11)
-        contourf(ax[2,0],Tmaxvals,levels)
-        ax[2,0].set_xlabel("Aspect ratio (-)")
-        ax[2,0].set_ylabel("Wing area ($m^2$)")
-        ax[2,0].set_title("Maximum Thrust (N)")
+        cs = contourf(ax[2,1], Tmaxvals, levels)
+        fig.colorbar(cs, ax=ax[2,1])
+        ax[2,1].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[2,1].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[2,1].set_title("Maximum thrust, $T_{max}$ (N)", color="black", fontweight="bold")
 
         # Wing tip deflection
         levels = np.linspace(np.min(dbvals[dbvals > 0]), np.max(dbvals), 9)
-        contourf(ax[2,1],dbvals,levels)
-        ax[2,1].set_xlabel("Aspect ratio (-)")
-        #ax[2,1].set_ylabel("Wing area ($m^2$)")
-        ax[2,1].set_title("Wing Tip Deflection (-)")
+        cs = contourf(ax[3,0], dbvals, levels)
+        fig.colorbar(cs, ax=ax[3,0])
+        ax[3,0].set_xlabel("Aspect ratio, AR (-)", color="black", fontweight="light")
+        ax[3,0].set_ylabel("Wing area, S ($m^2$)", color="black", fontweight="light")
+        ax[3,0].set_title("Wing tip deflection, $\\delta/b$ (-)", color="black", fontweight="bold")
+
+        # Hide unused subplot
+        ax[3,1].axis("off")
 
         suptitle = f"$V_{{opt}} = {obj_opt:.2f}$ m/s, $AR_{{opt}} = {ARopt:.1f}$, $S_{{opt}} = {Sopt:.3f}$ m$^2$, \n $C_{{L_{{\\mathrm{{des}}}}}} = {aircraft.CLdes:.2f}$, $\\lambda = {aircraft.taper:.2f}$, $\\tau = {aircraft.tau:.2f}$, $(\\delta/b)_{{\\mathrm{{max}}}} = {aircraft.dbmax:.2f}$"
         fig.suptitle(suptitle)
@@ -164,34 +181,44 @@ if __name__ == "__main__":
     aircraft = UEFC()
 
     # Payload weight
-    aircraft.mpay_g   = np.nan     # payload weight in grams
+    aircraft.mpay_g   = 250     # payload weight in grams
 
     # Geometry parameters
     S  = np.nan                  # Wing area (m^2)
     AR = np.nan                  # Wing aspect ratio
-    aircraft.taper    = np.nan   # taper ratio
-    aircraft.dihedral = np.nan   # Wing dihedral (degrees)
-    aircraft.tau      = np.nan   # thickness-to-chord ratio
+    aircraft.taper    = 0.7   # taper ratio
+    aircraft.dihedral = 10   # Wing dihedral (degrees)
+    aircraft.tau      = 0.10   # thickness-to-chord ratio
 
     # Tail parameters
-    aircraft.Sh = np.nan # Wing area of horizontal tail (m^2)
-    aircraft.Sv = np.nan # Wing area of vertical tail (m^2)
+    aircraft.Sh = 0.04 # Wing area of horizontal tail (m^2)
+    aircraft.Sv = 0.03 # Wing area of vertical tail (m^2)
 
     # Fuselage parameters
-    aircraft.l_AR = np.nan  # Fuselage wingspan to length ratio (-)
+    aircraft.l_AR = 1.63  # Fuselage wingspan to length ratio (-)
 
     # Aerodynamic parameters
-    aircraft.CLdes    = np.nan    # maximum CL wing will be designed to fly at (in cruise)
-    aircraft.e0       = np.nan    # Span efficiency for straight level flight
+    aircraft.CLdes    = 0.75    # maximum CL wing will be designed to fly at (in cruise)
+    aircraft.e0       = 1.00    # Span efficiency for straight level flight
 
     # Wing bending and material properties
-    aircraft.dbmax    = np.nan  # tip displacement bending constraint
+    aircraft.dbmax    = 0.08  # tip displacement bending constraint
     aircraft.rhofoam  = 32.     # kg/m^3. high load foam
     aircraft.Efoam    = 19.3E6  # Pa.     high load foam
 
     num_division = 41
-    scan_ARS(aircraft, np.nan, np.nan, np.nan, np.nan, num_division, show_plots=True)
 
+       # Coarse scan
+    # scan_ARS(aircraft, 4.0, 10.0, 0.10, 0.35, 21, show_plots=True)
 
+    # # Refined scan
+    # scan_ARS(aircraft, 5.0, 7.0, 0.15, 0.22, 41, show_plots=True)
 
+    # # Refined scan 2
+    # scan_ARS(aircraft, 5.2, 6.2, 0.145, 0.185, 51, show_plots=True)
 
+    # # Refined scan 3
+    # scan_ARS(aircraft, 5.3, 5.7, 0.150, 0.160, 61, show_plots=True)
+
+    # Final scan
+    scan_ARS(aircraft, 5.3, 5.7, 0.150, 0.159, 61, show_plots=True)
